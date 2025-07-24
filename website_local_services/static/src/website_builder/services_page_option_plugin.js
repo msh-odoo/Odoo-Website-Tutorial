ea48b329827cc286d528f702eb9c76c18739c57e
Mohammed Shekha <msh@odoo.com>
2025-07-25 00:50:09 +0530

    [FIX] Fixed overlay unselected after save option

--- website_local_services/static/src/website_builder/services_page_option_plugin.js
+++ website_local_services/static/src/website_builder/services_page_option_plugin.js
@@ -5,12 +5,12 @@ import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 
 class ServicesPageOption extends Plugin {
-    static id = "ServicesPageOption";
+    static id = "servicesPageOption";
     resources = {
         builder_options: [
             {
                 template: "website_local_services.ServicesPageOption",
-                selector: "div.js_services:has(#o_services_index_content)",
+                selector: "main:has(#o_services_index_content)",
                 editableOnly: false,
                 // reloadTarget: true,
                 title: _t("Service Page"),
