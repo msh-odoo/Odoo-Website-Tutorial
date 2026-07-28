946beb4ffe3e705299d3a49beedf333c583b2114
Mohammed Shekha <msh@odoo.com>
2026-07-28 19:49:23 +0530

    [ADD] website_event_track_oxp: Add module website_event_track_oxp

---
+++ website_event_track_oxp/__manifest__.py
@@ -0,0 +1,14 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Advanced Events OXP',
+    'category': 'Marketing',
+    'summary': 'Sponsors, Tracks, Agenda, Event News',
+    'version': '1.0',
+    'depends': ['website_event_track'],
+    'data': [],
+    'demo': [],
+    'assets': {},
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
